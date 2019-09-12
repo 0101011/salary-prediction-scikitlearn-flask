4b0c598ea582dbfda020c910ce351c3e3f1d3af3
Andrew Stepin <andrew.stepin@gmail.com>
2019-09-13 00:29:57 +0300

    convert func

--- model.py
+++ model.py
@@ -8,3 +8,9 @@ dataset["experience"].fillna(0, inplace=True)
 dataset["test_score"].fillna(dataset["test_score"].mean(), inplace=True)
 
 X = dataset.iloc[:, :3]
+
+# Converting strings to integer values.
+def convert_to_int(word):
+	word_dict = ['zero':0, 'one':1, 'two':2, 'three':3, 'four':4,
+	              'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9]
+	return word_dict[word]
