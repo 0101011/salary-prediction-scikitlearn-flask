048e37ccb768e4092511ad841abf65b4d861f4a4
Andrew Stepin <andrew.stepin@gmail.com>
2019-09-13 00:31:08 +0300

    dict fix

--- model.py
+++ model.py
@@ -11,6 +11,6 @@ X = dataset.iloc[:, :3]
 
 # Converting strings to integer values.
 def convert_to_int(word):
-	word_dict = ['zero':0, 'one':1, 'two':2, 'three':3, 'four':4,
-	              'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9]
+	word_dict = {'zero':0, 'one':1, 'two':2, 'three':3, 'four':4,
+	              'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9}
 	return word_dict[word]
